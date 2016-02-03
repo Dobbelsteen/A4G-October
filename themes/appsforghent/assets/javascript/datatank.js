12cce487c049e2a9647b9a28070ccd1ffe209ccd
Sandmeert <satanderm@gmail.com>
2016-02-03 11:23:07 +0100

    Added loading of datasets on the library page + filtering

---
+++ themes/appsforghent/assets/javascript/datatank.js
@@ -0,0 +1,93 @@
+$(function() { 
+    /**
+     * Load datasets
+     */
+    loadDatasets();
+    
+    function loadDatasets() {
+        $.ajax({
+            url: 'http://datatank.stad.gent/4/api/info',
+            method: 'GET',
+            success: function (data) {
+                console.log(data);
+                renderDatasets(data);
+            },
+            error: function (e) {
+                console.log(e);
+            },
+            cache: true
+        });
+    }
+    
+    function renderDatasets(data) {
+        var currentDiv = document.getElementById('datatank'); 
+        var themes = [];
+        for(var dataset in data) {
+            var theme = data[dataset].theme;
+            var extractName = dataset.split('/');
+            
+            // Create all elements needed for the dataset cards
+            var colDiv = document.createElement('div'); 
+            var cardDiv = document.createElement('div'); 
+            var cardSpan = document.createElement('div'); 
+            var cardSubspan = document.createElement('span'); 
+            var setName = document.createTextNode(extractName[1]); 
+            var setType = document.createTextNode(theme);
+             
+            // Set required CSS classes
+            colDiv.setAttribute('class', 'col-xs-6 col-sm-4');
+            cardDiv.setAttribute('class', 'card-white dataset-card');
+            cardDiv.setAttribute('data-url', dataset);
+            cardDiv.setAttribute('data-theme', theme);
+            cardSpan.setAttribute('class', 'data-type');
+            cardSubspan.setAttribute('class', 'data-subtype');
+            // Append elements to eachother and DOM
+            currentDiv.appendChild(colDiv); 
+            colDiv.appendChild(cardDiv);
+            cardDiv.appendChild(cardSpan);
+            cardSpan.appendChild(setName);
+            cardSubspan.appendChild(setType);
+            cardSpan.appendChild(cardSubspan);
+            
+            // Check for all themes in the datasets
+            var f = themes.indexOf(theme);
+            if(f < 0) {
+                themes.push(theme);
+            }
+        }
+        
+        // Render theme categories on the left side
+        var l = themes.length;
+        themes.sort();
+        var catP = document.getElementById('datatank-categories');
+
+        for(var i = 0; i < l; i++) {
+            var span = document.createElement('span'); 
+            var cat = document.createTextNode(themes[i]);
+            var br = document.createElement('br');
+            span.setAttribute('class', 'dataset-category-filter');
+            span.appendChild(cat);
+            catP.appendChild(span);
+            catP.appendChild(br);
+        }
+        
+        // Redirect to dataset on TheDatatank
+        $('.dataset-card').on('click', function(el) {
+            window.open('http://datatank.stad.gent/' + this.getAttribute('data-url'), '_blank');
+        });
+        
+        // Filter datasets by category
+        $('.dataset-category-filter').on('click', function(el) {
+            var selectedCat = el.target.innerText;
+            
+            if(selectedCat == 'Alle') {
+                $('.dataset-card').parent().show();
+            } else {
+                $('.dataset-card').parent().hide();
+                var sel = $(".dataset-card[data-theme='" + selectedCat + "']");
+                sel.parent().show();
+            }
+        });
+    }
+  
+});
--- themes/appsforghent/assets/javascript/hackathon.js
+++ themes/appsforghent/assets/javascript/hackathon.js
@@ -14,13 +14,12 @@ $(function() {
     }
   });
   
-  
   /**
      * This part handles the highlighting functionality.
      * We use the scroll functionality again, some array creation and 
      * manipulation, class adding and class removing, and conditional testing
      */
-    var aChildren = $(".nav-links").children(); // find the a children of the list items
+    var aChildren = $('.nav-links').children(); // find the a children of the list items
     var aArray = []; // create the empty aArray
     for (var i=0; i < aChildren.length; i++) {    
         var aChild = aChildren[i];
@@ -39,21 +38,20 @@ $(function() {
                 var divPos = $(theID).offset().top - 75; // get the offset of the div from the top of page
                 var divHeight = $(theID).height(); // get the height of the div in question
                 if (windowPos >= divPos && windowPos < (divPos + divHeight)) {
-                    $("a[href='" + theID + "']").addClass("active");
+                    $("a[href='" + theID + "']").addClass('active');
                 } else {
-                    $("a[href='" + theID + "']").removeClass("active");
+                    $("a[href='" + theID + "']").removeClass('active');
                 }
             }
 
             if(windowPos + windowHeight == docHeight) {
-                if (!$("nav li:last-child a").hasClass("active")) {
-                    var navActiveCurrent = $(".active").attr("href");
-                    $("a[href='" + navActiveCurrent + "']").removeClass("active");
-                    $("nav li:last-child a").addClass("active");
+                if (!$('nav li:last-child a').hasClass('active')) {
+                    var navActiveCurrent = $('.active').attr('href');
+                    $("a[href='" + navActiveCurrent + "']").removeClass('active');
+                    $('nav li:last-child a').addClass('active');
                 }
             }
-        }
-        
+        }    
     });
     
     /**
